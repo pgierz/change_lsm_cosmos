3ec963e1b78f503f0df03002b2efb20b27284bec
pgierz <paulgierz@gmail.com>
2017-05-25 19:12:55 +0200

    sets up script, does nothing yet

---
+++ change_lsm_cosmos.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -e
+################################################################################
+# Paul Gierz, May 2017
+#
+# This script performs all necessary steps for changing the land sea mask in a cosmos simulation.
+#
+# The following is preassumed:
+#     * A changed topojj file
+#     * A cosmos-o simulation using this file
+#
+################################################################################
